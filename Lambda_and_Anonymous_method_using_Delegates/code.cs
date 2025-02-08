a7229c45165d87aea4c306bed6b7bcffc492115d
BKR70 <bilas.duet.204027@gmail.com>
2025-02-08 23:51:28 +0600

    lambda and anonymous method folder & file added

---
+++ Lambda_and_Anonymous_method_using_Delegates/code.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_and_Anonymous_method_using_delegates
+{
+    internal class Program
+    {
+        public delegate int Calculation(int x, int y);
+        static void Main(string[] args)
+        {
+            //Anonymous method
+            Calculation cal = delegate(int x, int y) {
+                return x + y; 
+            };
+            Result(cal, 10, 30);
+
+            // Lambda expression 
+            Calculation cal2 = (x, y) => x + y;
+            Result(cal2, 10, 30);
+
+            // Lambda statements
+            Calculation cal3 = (x, y) =>
+            {
+                return x + y; 
+            };
+            Result(cal3, 10, 30);
+        }
+        static void Result(Calculation cal, int n1, int n2)
+        {
+            Console.WriteLine(cal(n1, n2));
+        }
+    }
+}
