56798c825d4b9e913d3428bbf286ae53487d5069
BKR70 <bilas.duet.204027@gmail.com>
2025-06-01 12:51:31 +0600

    Enums added

---
+++ Enums/EnumExample/EnumExample/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExample
+{
+    enum OrderStatus
+    {
+        OrderPlace = 5,
+        OrderConfirmed,
+        Packaging,
+        Delivering,
+        Received
+    }
+}
