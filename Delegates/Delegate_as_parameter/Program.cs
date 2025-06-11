6de79c51a6afe1e3e006a7f893b290899d8d0897
BKR70 <bilas.duet.204027@gmail.com>
2025-06-11 12:51:52 +0600

    Delegates files added

---
+++ Delegates/Delegate_as_parameter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_as_parameter
+{
+    internal class Program
+    {
+        public delegate int Calculation(int x, int y);
+        static void Main(string[] args)
+        {
+            Calculation cal = Sum;
+            Result(cal, 30, 40);
+            cal = Sub;
+            Result(cal, 70, 35);
+        }
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        static void Result(Calculation cal, int n1, int n2)
+        {
+            Console.WriteLine(cal(n1, n2));
+        }
+    }
+}
