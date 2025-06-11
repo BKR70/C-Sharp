307dda09ca8e08ac5c8b416a52ee0975d74c6d41
BKR70 <bilas.duet.204027@gmail.com>
2025-06-11 12:53:56 +0600

    Enums added

---
+++ Enums/EnumExample/EnumExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(OrderStatus.OrderPlace);
+            Console.WriteLine((int)OrderStatus.OrderPlace);
+            Console.WriteLine((int)OrderStatus.OrderConfirmed);
+            Console.WriteLine((int)OrderStatus.Packaging);
+            Console.WriteLine((int)OrderStatus.Delivering);
+            Console.WriteLine((int)OrderStatus.Received);
+            int productOrderInfo = 1;
+            if(productOrderInfo == (int)OrderStatus.OrderPlace)
+            {
+
+            }
+            else if(productOrderInfo == (int)OrderStatus.Packaging)
+            {
+
+            }
+
+            Console.ReadKey(); 
+        }
+    }
+}
